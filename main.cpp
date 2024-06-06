5b27e31c910a1f8f3e8af490c0a78a21e41bf0c0
Agyei Bacchus <151400661+Yage0110@users.noreply.github.com>
2024-06-06 12:32:47 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// main.cpp
+#include <iostream>
+#include "MemoryGame.hpp"
+
+using namespace std;
+
+int main() {
+    // Initialize the game
+    MemoryGame game;
+    
+    // Start the game
+    game.play();
+
+    return 0;
+}
